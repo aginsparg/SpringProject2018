3f65df34bc6351d366b898a1015ab0ae8baebea2
aginsparg <alginsparg@gmail.com>
2018-06-07 14:19:37 -0500

    first commit

--- src/main/java/edu/luc/cs271/linkedstack/LinkedQueue.java
+++ src/main/java/edu/luc/cs271/linkedstack/LinkedQueue.java
@@ -110,26 +110,32 @@ public class LinkedQueue<E> implements MyQueue<E> {
         int full_times_through_loop = 0;
         boolean lessthannext = false;
 
+
+//if the node following the one presently testing is not null and obj is still not in its correct spot the following is done.
         while (nodetesting.next!=null && !done) {
+
+// if the emergency level of the nodetesting is less than that of obj (ex: 5 and 6)it goes to the next node and updates the nodeInsertAfter
+// to be the node that was just tested.
             if (Integer.parseInt(testing.toString().substring(0, 1)) < Integer.parseInt(obj.toString().substring(0, 1))) {
                 nodeInsertAfter = nodetesting;
                 nodetesting = nodetesting.next;
                 testing = nodetesting.data;
                 full_times_through_loop++;
-            } else {
+            }
+// if the emergency level of the nodetesting is not less than the emergency level of obj then:
+// if the program has not yet done a full loop and the emergency level of nodetesting is not equal to that of obj then this is the front of the
+//queue and obj is inserted as the new beginning of the queue.    ex: 6 and 5
+// if it didn't yet do a full loop or the two numbers are equal then if the numbers are equal keep moving further
+// down the queue until the two numbers are not equal and then insert obj before the number to which it is not equal
+// if the two numbers do not satisfy any of the above situations, then insert a node before the node testing.
+            else {
                 if((full_times_through_loop ==0) && (Integer.parseInt(testing.toString().substring(0, 1)) != Integer.parseInt(obj.toString().substring(0, 1))))
                 {
                     Node<E> newfirst = new Node<>(obj, nodetesting);
                     first = newfirst;
                 }
-                else
-                    if((full_times_through_loop ==0) && (Integer.parseInt(nodetesting.next.data.toString().substring(0, 1)) > Integer.parseInt(obj.toString().substring(0, 1))))
-                    {
-                        nodetesting.next = new Node<>(obj, nodetesting.next);
-                    }
-                    else
-
-                        if(Integer.parseInt(testing.toString().substring(0, 1)) == Integer.parseInt(obj.toString().substring(0, 1))){
+                  else
+                       if(Integer.parseInt(testing.toString().substring(0, 1)) == Integer.parseInt(obj.toString().substring(0, 1))){
                             nodeInsertAfter = nodetesting;
                             nodetesting = nodetesting.next;
                             testing = nodetesting.data;
@@ -139,7 +145,6 @@ public class LinkedQueue<E> implements MyQueue<E> {
                                     nodeInsertAfter = nodetesting;
                                     nodetesting = nodetesting.next;
                                     testing = nodetesting.data;
-
                                 }
                                 else
                                 {lessthannext = true;}
@@ -153,6 +158,14 @@ public class LinkedQueue<E> implements MyQueue<E> {
                 done = true;
             }
         }
+
+        // if the node after the one testing is null and nodetesting is larger than obj and the nodetesting is the first node in the queue
+        // (which occurs if nodeInsertAfter is still null), insert obj as the new first
+        // if the node after the one testing is null and nodetesting is larger than obj and nodetesting is not the first node in the queue
+        // (nodeInsertAfter is not null) place the new node before the nodetesting
+        // if the node after the one testing is null and nodetesting is smaller than obj insert obj after nodetesting
+
+        // This block of code was done to avoid nullPointerException.
 
         if (nodetesting.next == null) {
             if ((Integer.parseInt(testing.toString().substring(0, 1)) > Integer.parseInt(obj.toString().substring(0, 1)) && (nodeInsertAfter == null))) {
